bbee8fc8853340476f7e99f0052a0f961fe3d5e5
shvyrkov.yuri <shvyrkov@yandex.ru>
2018-09-16 13:46:13 +0300

    1.3. JavaDoc. Code convention [#4087][#71521]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
+/**
+* Calculate - класс для вывода на экран надписи "Hello World".
+*
+* @author Yuri Shvyrkov (mailto:shvyrkov@yandex.ru)
+* @version 1.0
+* @since 2018.09.16
+*/
 package ru.job4j.calculate;
 
  public class Calculate {
-
+/**
+*Main.
+*@param args - letters.
+*/
   public static void main (String[] args) {
 
 	System.out.println("Hello World");
