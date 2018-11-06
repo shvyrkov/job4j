d7a9bb01a0d3b6ad387f5e7910c0d8f70544caa0
shvyrkov.yuri <shvyrkov@yandex.ru>
2018-11-06 21:30:28 +0300

    3.4. Расстояние между точками в системе координат[#83519]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,4 +1,4 @@
-package ru.job4j.condition;
+ package ru.job4j.condition;
 
 /**
  * Задание точки в системе координат и вычисление расстояние до другой точки.
@@ -31,33 +31,35 @@ public class Point {
 * @return result - расстояние до заданной точки
 */		
 	public double distanceTo(Point that) {
-//Точка А - текущая точка.	
+//Точка А - текущая точка. К ней мы обращаемся через оператор this.
 		Point a = this;
-//Точка В - это входящая точка. К ней мы можем обратиться напрямую через имя переменной.
+//Точка В - это входящая точка. К ней мы можем обратиться напрямую через имя переменной that.
 		Point b = that;
-    
+//Задание координат текущей точки а.    
 		int x1 = a.x;
 		int y1 = a.y;
+//Задание координат входящей точки b. 		
 		int x2 = b.x;
 		int y2 = b.y;
-		double result = Math.sqrt(
+//Вычисление расстояния между точками.		
+    return Math.sqrt(
             Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
 		);
-    return result;
 	}
 	
 	public static void main(String[] args) {
-//Определение объектов а и в с заданными координатами.
+//Определение объектов a и b с заданными координатами.
 		Point a = new Point(0, 1);
 		Point b = new Point(2, 5);
-// Вывод на консоль координат точек а и в.
+// Вывод на консоль координат точек a и b.
 		System.out.println("x1 = " + a.x);
 		System.out.println("y1 = " + a.y);
 		System.out.println("x2 = " + b.x);
 		System.out.println("y2 = " + b.y);
 //Вызов метода для определения расстояния между точками и его вывод на консоль.
     double result = a.distanceTo(b);
-    System.out.println("Расстояние между точками А и В : " + result);
-	}
+//    System.out.println("Расстояние между точками А и В : " + result);
+		System.out.print("Distance between A and B : " + result);
+ 	}
 	
 	}
