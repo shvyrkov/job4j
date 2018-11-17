c4b3ec5d2ac2a52824fd63ceaadc1d08145b4873
Yuri Shvyrkov <shvyrkov@yandex.ru>
2018-11-17 19:20:22 +0400

    5.5. Рефакторинг кода. [#33459][#88658]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,47 +1,78 @@
 package ru.job4j.loop;
 
+	import java.util.function.BiPredicate;
+	
 /**
  *Класс Paint для построения пирамиды заданной высоты в псевдографике.
- *
+ *Рефакторинг.
  *@author Shvyrkov Yuri (mailto: shvyrkov@yandex.ru)
- *@version 1.0
- *@since 2018.11.16
+ *@version 1.1
+ *@since 2018.11.17
  */
- 
 public class Paint {
-    /**
-     * Метод pyramid для построения пирамиды заданной высоты.
-     *
-     * @param height - высота пирамиды.
-     * @return - пирамида заданной высоты в псевдографике.
-     */
+	
+/**
+* Метод rightTrl для построения правосторонней пирамиды заданной высоты.
+* @param height - высота пирамиды.
+* @return - правосторонняя пирамида.
+*/	
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+/**
+* Метод leftTrl для построения левосторонней пирамиды заданной высоты.
+* @param height - высота пирамиды.
+* @return - правосторонняя пирамида.	
+*/
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+/** Метод pyramid для построения пирамиды заданной высоты.
+* @param height - высота пирамиды.
+* @return - пирамида заданной высоты в псевдографике.
+*/ 
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+/** Метод loopBy для перебора ячеек в квадрате с шириной weight и высотой height
+* с выставлением в ячейке "^" или пробела в зависимости от условия для row  и column.
+* @param height - высота пирамиды.
+* @param weight - ширина пирамиды.
+* @return - пирамида заданной высоты в псевдографике.
+*/ 
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-	// Ширина = 2 высоты минус 1 клетка(центр).	
-        int weight = 2 * height - 1;
-	// Внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
-	// Внутренний цикл определяет положение ячейки в строке.		
             for (int column = 0; column != weight; column++) {
-	// если строка удовлетворяет условию ячейки (слева и справа), то рисуем галку. 			
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
                 }
             }
-	// Добавляем перевод строки.		
             screen.append(System.lineSeparator());
         }
         return screen.toString();
     }
-
-/**
+}	
+	
+/*
 * Метод rightTrl для построения правосторонней пирамиды заданной высоты.
-*
 * @param height - высота пирамиды.
 * @return - правосторонняя пирамида.
-*/
+*
     public String rightTrl(int height) {
         // Буфер для результата.
         StringBuilder screen = new StringBuilder();
@@ -65,12 +96,11 @@ public class Paint {
         // Получаем результат.
         return screen.toString();
     }
-/**	
+**	
 * Метод leftTrl для построения левосторонней пирамиды заданной высоты.
-*
 * @param height - высота пирамиды.
 * @return - правосторонняя пирамида.	
-*/
+*
 	public String leftTrl(int height) {
 		StringBuilder screen = new StringBuilder();
 		int weight = height;
@@ -87,3 +117,28 @@ public class Paint {
 		return screen.toString();
 	}
 }
+** Метод pyramid для построения пирамиды заданной высоты.
+* @param height - высота пирамиды.
+* @return - пирамида заданной высоты в псевдографике.
+*
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+	// Ширина = 2 высоты минус 1 клетка(центр).	
+        int weight = 2 * height - 1;
+	// Внешний цикл двигается по строкам.
+        for (int row = 0; row != height; row++) {
+	// Внутренний цикл определяет положение ячейки в строке.		
+            for (int column = 0; column != weight; column++) {
+	// если строка удовлетворяет условию ячейки (слева и справа), то рисуем галку. 			
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+	// Добавляем перевод строки.		
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+*/
