f297f7ab67c37af9cc8d7416118ff85a08b02049
Yuri Shvyrkov <shvyrkov@yandex.ru>
2018-11-22 10:17:50 +0400

    Замена ^ на != .6.3. Массив заполнен true или false [#53857][#91116]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -16,7 +16,7 @@ package ru.job4j.array;
 	public boolean mono(boolean[] data) {
         boolean result = false;
         for (int i = 0; i < data.length; i++) {
-			if (data[0] ^ data[i]) {
+			if (data[0] != data[i]) {
 				result = false;
 				break;
 			} else {
