4d176602edb49864e0321a00617d67ed89c3e224
Yuri Shvyrkov <shvyrkov@yandex.ru>
2018-11-23 11:24:34 +0400

    6.5. Отсортированные ячейки не проверяются

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -19,7 +19,7 @@ public class BubbleSort {
 	public int[] sort(int[] array) {
 		int temp = 0;		
 		for (int i = 0; i < array.length - 1; i++) {			
-			for (int j = 0; j < array.length - 1; j++) {
+			for (int j = 0; j < array.length - i - 1; j++) {
 				if (array[j] > array[j + 1]) {					
 					temp = array[j];
 					array[j] = array[j + 1];
