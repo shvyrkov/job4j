195948b5dce3c9adc220dd86b283daea8b04ad2e
Yuri Shvyrkov <shvyrkov@yandex.ru>
2019-01-25 13:51:01 +0400

    3. StartUITest, StubInput. Test Delete for whole array  [#785][#108365]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -73,21 +73,39 @@ public class StartUITest {
         Item item3 = tracker.add(new Item("test_name-3", "desc_3")); //Добавляем заявку 3 в ячейку [2]
         Item item4 = tracker.add(new Item("test_name-4", "desc_4")); //Добавляем заявку 4 в ячейку [3]
 
-        Tracker expect = new Tracker();     // создаём Tracker для сравнения
-//        Item item1e = expect.add(new Item("test_name-1", "desc_1")); //Добавляем заявку 1 в ячейку [0]
-        Item item2e = expect.add(new Item("test_name-2", "desc_2")); //Добавляем заявку 2 в ячейку [1]
-        Item item3e = expect.add(new Item("test_name-3", "desc_3")); //Добавляем заявку 3 в ячейку [2]
-        Item item4e = expect.add(new Item("test_name-4", "desc_4")); //Добавляем заявку 4 в ячейку [3]
+        Tracker expect1 = new Tracker();     // создаём Tracker для сравнения
+//        Item item1e1 = expect1.add(new Item("test_name-1", "desc_1")); //Добавляем заявку 1 в ячейку [0]
+        Item item2e1 = expect1.add(new Item("test_name-2", "desc_2")); //Добавляем заявку 2 в ячейку [1]
+        Item item3e1 = expect1.add(new Item("test_name-3", "desc_3")); //Добавляем заявку 3 в ячейку [2]
+        Item item4e1 = expect1.add(new Item("test_name-4", "desc_4")); //Добавляем заявку 4 в ячейку [3]
 
         Input input00 = new StubInput(new String[]{"3", item1.getId(), "Y", "6"});   // Удаление item1 из ячейки [0].
         new StartUI(input00, tracker).init();     //   создаём StartUI и вызываем метод init()
-
         for (int i = 0; i < tracker.findAll().length; i++) {
-            assertThat(tracker.findAll()[i].getName(), is(expect.findAll()[i].getName())); // проверяем весь массив после удаления заявки
+            assertThat(tracker.findAll()[i].getName(), is(expect1.findAll()[i].getName())); // проверяем весь массив после удаления заявки
         }
-
-//        Input input11 = new StubInput(new String[]{"3", item3.getId(), "Y", "6"});   // Удаление item3 из ячейки [1], где она оказалась после предыдущего удаления.
-//        new StartUI(input1, tracker).init();     //   создаём StartUI и вызываем метод init()
-//        assertThat(tracker.findAll()[1].getName(), is("test_name-4")); // проверяем, ячейка [1] в трекере содержит имя из ячейки [2], т.е.произошёл сдвиг влево.
+		
+        Tracker expect2 = new Tracker();     // создаём Tracker для сравнения
+//        Item item1e2 = expect2.add(new Item("test_name-1", "desc_1")); //Добавляем заявку 1 в ячейку [0]
+        Item item2e2 = expect2.add(new Item("test_name-2", "desc_2")); //Добавляем заявку 2 в ячейку [1]
+//        Item item3e2 = expect2.add(new Item("test_name-3", "desc_3")); //Добавляем заявку 3 в ячейку [2]
+        Item item4e2 = expect2.add(new Item("test_name-4", "desc_4")); //Добавляем заявку 4 в ячейку [3]
+		
+        Input input11 = new StubInput(new String[]{"3", item3.getId(), "Y", "6"});   // Удаление item3 из середины массива.
+        new StartUI(input11, tracker).init();     //   создаём StartUI и вызываем метод init()
+		for (int i = 0; i < tracker.findAll().length; i++) {
+            assertThat(tracker.findAll()[i].getName(), is(expect2.findAll()[i].getName())); // проверяем весь массив после удаления заявки
+        }
+        Tracker expect3 = new Tracker();     // создаём Tracker для сравнения
+//        Item item1e3 = expect3.add(new Item("test_name-1", "desc_1")); //Добавляем заявку 1 в ячейку [0]
+        Item item2e3 = expect3.add(new Item("test_name-2", "desc_2")); //Добавляем заявку 2 в ячейку [1]
+//        Item item3e3 = expect3.add(new Item("test_name-3", "desc_3")); //Добавляем заявку 3 в ячейку [2]
+//        Item item4e3 = expect3.add(new Item("test_name-4", "desc_4")); //Добавляем заявку 4 в ячейку [3]
+		
+        Input input22 = new StubInput(new String[]{"3", item4.getId(), "Y", "6"});   // Удаление item4 из последней ячейки массива
+        new StartUI(input22, tracker).init();     //   создаём StartUI и вызываем метод init()
+		for (int i = 0; i < tracker.findAll().length; i++) {
+            assertThat(tracker.findAll()[i].getName(), is(expect3.findAll()[i].getName())); // проверяем весь массив после удаления заявки
+        }		
     }
 }
