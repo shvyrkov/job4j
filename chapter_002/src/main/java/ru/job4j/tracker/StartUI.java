85997d585d4d2fc3e8150119a7173d944721bd0f
shvyrkov.yuri <shvyrkov@yandex.ru>
2019-01-30 21:28:45 +0300

    4.6 StartUITestRef for 'List of All Items'

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -118,7 +118,7 @@ public class StartUI {
                 System.out.println("--------------------------------------------------");
             }
         }
-        System.out.println("----------------- End of list -------------------");
+        System.out.println("----------------- End of list --------------------");
     }
 
     /**
